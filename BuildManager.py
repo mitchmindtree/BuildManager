faba60fe009ee05edd697acfecfee3be34980c1f
mitchmindtree <mitchell.nordine@gmail.com>
2014-05-21 18:21:37 +1000

    remove useless tmp stuff

--- BuildManager.py
+++ BuildManager.py
@@ -28,6 +28,8 @@ Options:
 import os
 from docopt import docopt
 from HeaderChecker import updateHeaders
+from pprint import pprint
+import tmuxp
 
 
 def genMakeString(headerDiff):
@@ -51,8 +53,25 @@ def cleanPath(path):
         raise Exception("I can't find the given path! You're not leading me astray... are you?")
 
 
+def runInNewTmuxWindow(path):
+    serv = tmuxp.Server()
+    sesh = serv.getById('$0')
+    win = sesh.findWhere({"window_name" : "RUN_JEN"})
+    if win:
+        pane = win.attached_pane()
+    else:
+        win = sesh.new_window(attach=True, window_name="RUN_JEN")
+        pane = win.attached_pane()
+    pane.send_keys('cd '+path, enter=True)
+    pane.send_keys('make run', enter=True)
+
+
+def runInNewTab():
+    os.system("""osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "make run" in selected tab of the front window'""")
+
+
 def main():
-    args = docopt(__doc__, version='Header Checker -- SUPER RADICAL EDITION')
+    args = docopt(__doc__, version='Build Manager -- SUPER RADICAL EDITION')
     path = cleanPath(args['<makePath>'])
     headerDiff = updateHeaders(path)
     s = genMakeString(headerDiff)
@@ -60,9 +79,16 @@ def main():
     if (args['--run']):
         print("Running dis business...")
         try:
-            os.system("""osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "make run" in selected tab of the front window'""")
-        except:
-            os.system("make run")
+            runInNewTmuxWindow(path)
+        except Exception, e:
+            msg = str(e) + "\nNow trying to open in new tab instead..."
+            print(msg)
+            try:
+                runInNewTab()
+            except Exception, e:
+                msg = str(e) + "\nNow just going to run in Vim window instead..."
+                print(msg)
+                os.system("make run")
     else:
         print("No run command found, finishing up :-)")
 
